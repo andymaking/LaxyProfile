942c51b2ceb8e0cf9c7d5e167bb2beb64dd94e8e
Andima Udoh <andymaking@example.com>
2022-03-03 19:29:37 +0100

    update

--- app/src/main/java/io/king/laxyprofile/MainActivity.kt
+++ app/src/main/java/io/king/laxyprofile/MainActivity.kt
@@ -1,21 +1,15 @@
 package io.king.laxyprofile
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import io.king.laxyprofile.ui.theme.LaxyProfileTheme
 
 class MainActivity : ComponentActivity() {
